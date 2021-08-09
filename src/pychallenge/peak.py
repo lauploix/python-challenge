909ac93969974407836ed819e15746e2d551a12a
Laurent Ploix <plume@spotify.com>
2021-08-09 16:01:38 +0200

    Update peak.py

--- src/pychallenge/peak.py
+++ src/pychallenge/peak.py
@@ -1,8 +1,25 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
+URL = "http://www.pythonchallenge.com/pc/def/banner.p"
+from pprint import pprint
 
 
 def run():
-    pass
+    import pickle
+    import urllib.request
+
+    response = urllib.request.urlopen(URL).read()
+    o = pickle.loads(response)
+
+    res = ""
+
+    for line in o:
+        for tup in line:
+            ch = tup[0]
+            occ = tup[1]
+            res = res + ch * occ
+        res = res + "\n"
+
+    print(res)
 
 
 if __name__ == "__main__":
